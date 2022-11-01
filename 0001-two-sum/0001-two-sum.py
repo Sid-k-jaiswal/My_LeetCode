f9b92ba7ad3f1a650a6c3bea1ac6914da3611561
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2022-11-01 18:32:39 +0530

    Time: 128 ms (62.25%), Space: 15.1 MB (68.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,21 +3,13 @@ class Solution:
         #approach 1
         n = len(nums)
         
-        seen = {}
-        for i, value in enumerate(nums): #1
-            remaining = target - nums[i] #2
-            if remaining in seen: #3
-                return [i, seen[remaining]]  #4
-            else:
-                seen[value] = i  #5
+        my_dict = {}
         
-#         my_dict = {}
-        
-#         for i in range(n):
-#             sum = target-nums[i]
-#             if sum in my_dict:
-#                 return [my_dict[sum],i]
-#             my_dict[nums[i]] = i
+        for i, value in enumerate(nums):
+            sum = target-value
+            if sum in my_dict:
+                return [my_dict[sum],i]
+            my_dict[value] = i
 
 # i = 0, d = {3:0}
 # i = 1, 6-nums[1]:(6-2)=4, d = {3:0,2:1}
